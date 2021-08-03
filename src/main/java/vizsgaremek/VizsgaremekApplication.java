9104bf5614f99508f603f6d00df4d23b9f9b821c
kovacseni <kovacseni@gmail.com>
2021-08-03 13:07:05 +0200

    Initial commit

---
+++ src/main/java/vizsgaremek/VizsgaremekApplication.java
@@ -0,0 +1,13 @@
+package vizsgaremek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizsgaremekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VizsgaremekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vizsgaremek/VizsgaremekApplicationTests.java
@@ -0,0 +1,13 @@
+package vizsgaremek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizsgaremekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
