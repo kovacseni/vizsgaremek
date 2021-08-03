cd4bd14a3434a5ea0370173ed9f6377b8630f67e
kovacseni <kovacseni@gmail.com>
2021-08-03 13:20:39 +0200

    Környezet

--- src/main/java/vizsgaremek/VizsgaremekApplication.java
+++ src/main/java/vizsgaremek/VizsgaremekApplication.java
@@ -1,7 +1,9 @@
 package vizsgaremek;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VizsgaremekApplication {
@@ -10,4 +12,8 @@ public class VizsgaremekApplication {
         SpringApplication.run(VizsgaremekApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
